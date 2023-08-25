a20697c53eeec9d639f448bb016a42f13ea4dda7
Andrey Mudri <andreybeckert@gmail.com>
2023-08-25 16:04:56 -0300

    httpcode fix

--- src/publications/publications.repository.ts
+++ src/publications/publications.repository.ts
@@ -2,33 +2,41 @@ import { HttpException, Injectable } from '@nestjs/common';
 import { CreatePublicationDto } from './dto/create-publication.dto';
 import { UpdatePublicationDto } from './dto/update-publication.dto';
 import { PrismaService } from '../prisma/prisma.service';
-
+import { Publications as PublicationModel } from '@prisma/client';
 @Injectable()
 export class PublicationsRepository {
   constructor(private readonly prisma: PrismaService) {}
-  async create(createPublicationDto: CreatePublicationDto) {
+  async create(
+    createPublicationDto: CreatePublicationDto,
+  ): Promise<PublicationModel> {
     const create = this.prisma.publications.create({
       data: createPublicationDto,
     });
     return create;
   }
 
-  async findAll() {
+  async findAll(): Promise<PublicationModel[]> {
     const result = await this.prisma.publications.findMany();
     return result;
   }
 
-  async findOne(id: number) {
+  async findOne(id: number): Promise<PublicationModel> {
     const result = await this.prisma.publications.findUnique({
       where: { id },
     });
     return result;
   }
 
-  async update(id: number, updatePublicationDto: UpdatePublicationDto) {
+  async update(
+    id: number,
+    updatePublicationDto: UpdatePublicationDto,
+  ): Promise<PublicationModel> {
     const check = await this.findOne(id);
     if (!check) {
-      throw new HttpException('Publication not found', 404);
+      throw new HttpException('Publication not found', 403);
+    }
+    if (check.date < new Date()) {
+      throw new HttpException('Publication already posted', 404);
     }
     return this.prisma.publications.update({
       where: { id },
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,23 +2,50 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { PrismaService } from '../src/prisma/prisma.service';
 
+let app: INestApplication;
+let prisma: PrismaService;
+beforeEach(async () => {
+  const moduleFixture: TestingModule = await Test.createTestingModule({
+    imports: [AppModule],
+  }).compile();
+
+  app = moduleFixture.createNestApplication();
+  prisma = app.get(PrismaService);
+  await prisma.publications.deleteMany();
+  await prisma.posts.deleteMany();
+  await prisma.medias.deleteMany();
+  await app.init();
+});
 describe('AppController (e2e)', () => {
-  let app: INestApplication;
-
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile();
-
-    app = moduleFixture.createNestApplication();
-    await app.init();
-  });
-
-  it('/ (GET)', () => {
+  it('/health (GET)', () => {
     return request(app.getHttpServer())
-      .get('/')
+      .get('/health')
       .expect(200)
-      .expect('Hello World!');
+      .expect("I'm okay!");
   });
 });
+describe('PostsController (e2e)', () => {
+  it('/posts (GET)', () => {
+    return request(app.getHttpServer()).get('/posts').expect(200).expect([]);
+  });
+  it('/posts (POST)', () => {
+    return request(app.getHttpServer())
+      .post('/posts')
+      .send({ title: 'test', text: 'test' })
+      .expect(201);
+  });
+});
+describe('MediasController (e2e)', () => {
+  it('/medias (GET)', () => {
+    return request(app.getHttpServer()).get('/medias').expect(200).expect([]);
+  });
+  it('/medias (POST)', () => {
+    return request(app.getHttpServer())
+      .post('/medias')
+      .send({ title: 'test', username: 'test' })
+      .expect(201);
+  });
+});
+describe('publicationsController (e2e)', () => {});
